1cd99b317e359b9956739e9201afac66a1978b0a
JaviXR <JaviXR@outlook.es>
2023-02-11 15:55:48 +0100

    version traducida

--- models/client.py
+++ models/client.py
@@ -3,10 +3,10 @@ from odoo import models, fields, api
 
 class Order(models.Model):
     _name = 'frusec.client'
-    _description = 'Client template'
+    _description = 'Plantilla de cliente'
 
-    name = fields.Char(string='Name', required=True)
-    phone = fields.Char(string='Phone')
+    name = fields.Char(string='Nombre', required=True)
+    phone = fields.Char(string='Teléfono')
     contact = fields.Char(string='Email')
-    website = fields.Char(string='Website')
-    note = fields.Text(string='Note')
+    website = fields.Char(string='Web de contacto')
+    note = fields.Text(string='Anotación')
